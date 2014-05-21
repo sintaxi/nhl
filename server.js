e559807ee2e1c5325ca617536b97faa9c2d633ff
Brock Whitten <brock@chloi.io>
2014-05-21 10:07:46 -0500

    CORS

--- server.js
+++ server.js
@@ -8,19 +8,13 @@ app.get("/", function(req, rsp){
   rsp.send("Welcome to the makeshift nhl api\n")
 })
 
-app.configure(function(){
-  app.use(function(req, rsp, next){
-    rsp.header("Access-Control-Allow-Origin", "*")
-    rsp.header("Access-Control-Allow-Headers", "X-Requested-With")
-    next()
-  })
-});
-
 app.get("/:team/:season?", function(req, rsp){
+  rsp.header("Access-Control-Allow-Origin", "*")
+  rsp.header("Access-Control-Allow-Headers", "X-Requested-With")
   nhl.team(req.params.team, req.params.season || "20132014", function(players){
     if(players){
       var body = JSON.stringify(players, null, 2) + "\n"
-      rsp.send(body, {"Content-Type": "application/json"})
+      rsp.send(body)
     }else{
       rsp.send("stats not found\n", 404)
     }
